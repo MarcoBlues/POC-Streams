16aca23ff8c798e4c71a027d272e030e65687a5f
Marco Briatore <30911931+MarcoBriatore@users.noreply.github.com>
2019-03-25 22:32:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,30 +14,26 @@ public class Main {
             PersonList.add(new Persona(i*5,i*6000000));
         }
 
-        List<Persona> Aux = PersonList.
-                            stream().
-                            filter(Persona -> Persona.getEdad() > 21).
-                            collect(Collectors.toList());
-
-        List<Persona> Aux2 = PersonList.
-                stream().
-                filter(Persona -> Persona.getEdad() < 18).
-                collect(Collectors.toList());
-
-
-        List<Persona> Aux3 = PersonList.
-                stream().
-                filter(Persona -> Persona.getEdad() > 21).
-                filter(Persona -> Persona.getDni() > 20000000).
-                collect(Collectors.toList());
 
         System.out.println("\n______PERSONAS MAYORES A 21_______");
-        System.out.println(String.format("Persona %s" , Aux));
+        System.out.println(String.format("Persona %s" , PersonList.
+                                                        stream().
+                                                        filter(Persona -> Persona.getEdad() > 21).
+                                                        collect(Collectors.toList())));
 
         System.out.println("\n_______PERSONAS MENORES A 18_________");
-        System.out.println(String.format("Persona %s" , Aux2));
+        System.out.println(String.format("Persona %s" , PersonList.
+                                                        stream().
+                                                        filter(Persona -> Persona.getEdad() < 18).
+                                                        collect(Collectors.toList())));
+
+
         System.out.println("\n________PERSONAS MAYORES A 21 Y UN DNI MAYOR A 200000_________");
-        System.out.println(String.format("Persona %s" , Aux3));
+        System.out.println(String.format("Persona %s" , PersonList.
+                                                        stream().
+                                                        filter(Persona -> Persona.getEdad() > 21).
+                                                        filter(Persona -> Persona.getDni() > 20000000).
+                                                        collect(Collectors.toList())));
 
     }
 }
